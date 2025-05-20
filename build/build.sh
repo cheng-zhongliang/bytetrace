fe7b724198bf6ddb0d245180958454a53d0a8817
cheng zhongliang <cheng.zhongliang@h3c.com>
2025-05-20 09:18:58 +0800

    support arm64 arch

--- build/build.sh
+++ build/build.sh
@@ -4,6 +4,8 @@ set -ex
 
 home=$1
 
+arch=$2
+
 go mod tidy
 
 cd $home/pkg/bytetrace/
@@ -12,10 +14,10 @@ bpftool btf dump file /sys/kernel/btf/vmlinux format c > C/vmlinux.h
 
 go get github.com/cilium/ebpf/cmd/bpf2go
 
-go run github.com/cilium/ebpf/cmd/bpf2go -target amd64 -tags linux -go-package bytetrace tracepoint C/tracepoint.c
+go run github.com/cilium/ebpf/cmd/bpf2go -target $arch -tags linux -go-package bytetrace tracepoint C/tracepoint.c
 
 cd $home/cmd/
 
-go build -o $home/build/bytetrace
+CGO_ENABLED=0 GOARCH=$arch go build -o $home/build/bytetrace
 
 echo "Build OK"
--- makefile
+++ makefile
@@ -1,7 +1,10 @@
 all: bytetrace
 
 bytetrace:
-	cd build && ./build.sh $(CURDIR)
+	cd build && ./build.sh $(CURDIR) amd64
+
+arm64:
+	cd build && ./build.sh $(CURDIR) arm64
 
 clean:
 	cd build && rm bytetrace
